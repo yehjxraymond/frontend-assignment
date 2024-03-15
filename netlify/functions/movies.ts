f9ffb21fd5094dff6d7c7d125fa1fd9fc03716de
Raymond Yeh <ray@geek.sg>
2024-03-15 22:25:09 +0800

    fix: cors

--- netlify/functions/movies.ts
+++ netlify/functions/movies.ts
@@ -22,6 +22,9 @@ const handler: Handler = async (
       statusCode: 500,
       headers: {
         "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
       },
       body: JSON.stringify({ error: "Server error" }),
     };
