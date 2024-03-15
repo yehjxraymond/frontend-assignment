e93559fb31a755861dcc9ecc199f79948d700047
Raymond Yeh <ray@geek.sg>
2024-03-15 22:22:02 +0800

    fix: cors

--- netlify/functions/movie.ts
+++ netlify/functions/movie.ts
@@ -43,6 +43,9 @@ const handler: Handler = async (
       statusCode: 404,
       headers: {
         "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
       },
       body: JSON.stringify({ message: "Not found" }),
     };
--- netlify/functions/movies.ts
+++ netlify/functions/movies.ts
@@ -30,6 +30,9 @@ const handler: Handler = async (
       statusCode: 200,
       headers: {
         "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
       },
       body: JSON.stringify(movies.map(({ synopsisFull, ...movie }) => movie)),
     };
